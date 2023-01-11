62f07e4c60f9b99cedd0b502145f16bff85567d8
Ruslan Patyk <ruslan.patyk@medtronic.com>
2023-01-11 11:29:41 +0200

    Initial Commit

---
+++ HumtapLiveCoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HumtapLiveCoding
+//
+//  Created by Ruslan Patyk on 11.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
